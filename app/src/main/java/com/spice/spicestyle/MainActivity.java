172af650de9ebc5930b71ea38a0183ffb49e31aa
DESKTOP-35M89RH\yaashvi <yaashvisoftware@gmail.com>
2017-03-29 10:48:03 +0530

    bottom navigation bar

--- app/src/main/java/com/spice/spicestyle/MainActivity.java
+++ app/src/main/java/com/spice/spicestyle/MainActivity.java
@@ -1,13 +1,60 @@
 package com.spice.spicestyle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewIds();
+
+        setClickListenter();
     }
+
+    private void findViewIds(){
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+    }
+
+    private void setClickListenter(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_search:
+                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_cart:
+                        Toast.makeText(MainActivity.this, "Cart", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_account:
+                        Toast.makeText(MainActivity.this, "account", Toast.LENGTH_SHORT).show();
+                        break;
+                    /*case R.id.menu_more:
+                        Toast.makeText(MainActivity.this, "More", Toast.LENGTH_SHORT).show();
+                        break;*/
+
+
+                }
+
+
+                return true;
+            }
+        });
+    }
+
 }
