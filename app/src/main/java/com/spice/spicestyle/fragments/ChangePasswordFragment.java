bb9a3ab862c6edb130e3441186380bfbc61f64bd
Rakesh Yadav <yaashvisoftware@gmail.com>
2017-03-30 13:03:43 +0530

    Image

--- app/src/main/java/com/spice/spicestyle/fragments/ChangePasswordFragment.java
+++ app/src/main/java/com/spice/spicestyle/fragments/ChangePasswordFragment.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 
 import com.spice.spicestyle.R;
 
-public class FragmentChangePassword extends Fragment {
+public class ChangePasswordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View shoppingCartView = inflater.inflate(R.layout.fragment_registration, container, false);
